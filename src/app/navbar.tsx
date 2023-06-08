67f0eda062ff046be3822167f7c7a953b484313c
Samuel Alcântara Fontenele Rocha <samuel.rocha@dellead.com>
2023-06-08 18:49:55 -0300

    navbar style DONE!!

--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -17,36 +17,109 @@ type PlayerImageProps = {
   src: string;
   alt: string;
   className?: string;
+  onClick?: () => any;
+  Click?: any;
+  select?: any;
 };
 
-const PlayerImage = ({ src, alt, className }: PlayerImageProps) => (
-  <div className="w-1/4">
+const PlayerImage = ({ src, alt, className, onClick, select }: PlayerImageProps) => (
+  <div
+    className="w-1/4 flex flex-col items-center justify-center"
+  >
+    {select? (
+
+      <div
+      className= 'text-white mt-2 text-center mx-auto'>{alt}
+    </div>
+      ) : (<></>)}
+
     <Image
       src={src}
       width={100}
       height={100}
       alt={alt}
       className={`h-auto w-auto mx-auto ${className}`}
+      onClick={onClick}
     />
+
   </div>
 );
 
+type SelectedState = {
+  Enki: boolean;
+  Darce: boolean;
+  Cahara: boolean;
+  Rag: boolean;
+};
 
 
-const NavBar = () => (
-  
-  <div className="shadow-lg rounded-lg p-4 w-1/4">
-    <div className="h-20 relative">
-      <div className="w-full h-full flex items-center">
-        <PlayerImage src={Images.Enki} alt="Enki" className="rounded-l-lg" />
-        <PlayerImage src={Images.Darce} alt="D'arce" />
-        <PlayerImage src={Images.Cahara} alt="Cahara" />
-        <PlayerImage src={Images.Rag} alt="Ragnvaldr" className="rounded-r-lg" />
+
+
+const NavBar = () => {
+  const [Selected, setSelected] = useState<SelectedState>({
+    Enki: false,
+    Darce: false,
+    Cahara: false,
+    Rag: false,
+  });
+
+  const HandleClick = (x: string) => {
+
+    const updatedSelected: SelectedState = {} as SelectedState;
+    Object.keys(Selected).forEach((key) => {
+      updatedSelected[key as keyof SelectedState] = false; //all to false
+    });
+    updatedSelected[x as keyof SelectedState] = true;
+
+    setSelected(updatedSelected);
+  };
+
+  return (
+
+    <div className="shadow-lg rounded-lg p-4 w-1/4 bg-[url('/Dungeon_background.webp')] bg-center">
+      <div className="h-40 relative">
+        <div className="w-full h-full flex items-center">
+
+          <PlayerImage
+            onClick={() => HandleClick('Enki')}
+            src={Selected.Enki ? Images.Enki : Images.Enki2}
+            alt="Enki"
+            className="rounded-l-lg"
+            select={Selected.Enki}
+          />
+
+
+          <PlayerImage
+            onClick={() => HandleClick('Darce')}
+            src={Selected.Darce ? Images.Darce : Images.Darce2}
+            alt="D'arce"
+            select={Selected.Darce}
+          />
+
+
+          <PlayerImage
+            onClick={() => HandleClick('Cahara')}
+            src={Selected.Cahara ? Images.Cahara : Images.Cahara2}
+            alt="Cahara"
+            select={Selected.Cahara}
+          />
+
+
+          <PlayerImage
+            onClick={() => HandleClick('Rag')}
+            src={Selected.Rag ? Images.Rag : Images.Rag2}
+            alt="Ragnvaldr"
+            className="rounded-r-lg"
+            select={Selected.Rag}
+          />
+
+        </div>
       </div>
     </div>
-  </div>
- 
-  
-);
+
+  )
+
+
+}
 
 export default NavBar;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,6 @@ import NavBar from "./navbar";
 
 export default function Home() {
     return (
-        <div className="flex flex-col items-center justify-center min-h-screen py-2" >
         <NavBar />
-        </div>
     );
     }
